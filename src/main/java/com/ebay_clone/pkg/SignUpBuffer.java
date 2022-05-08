cb8898587b400648a962af012cec68fd145286e1
gabeq20 <104606882+gabeq20@users.noreply.github.com>
2022-05-08 15:58:20 -0400

    Sign up page functionality

---
+++ src/main/java/com/ebay_clone/pkg/SignUpBuffer.java
@@ -0,0 +1,37 @@
+package com.ebay_clone.pkg;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet("/signUpBufferServlet")
+public class SignUpBuffer extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+
+        try {
+            Statement stmt = con.createStatement();
+            String sql = "INSERT INTO users (username, password, email, permissions) VALUES (?, ?, ?, 1);";
+            PreparedStatement prep = con.prepareStatement(sql);
+
+            prep.setString(1, username);
+            prep.setString(2, password);
+            prep.setString(3, email);
+            prep.executeUpdate();
+            prep.close();
+
+            response.sendRedirect("home.jsp");
+
+          
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
